37fec9664508fdec9cbed58b6d4c0b6a63caba47
HyeonjongJang <lezelamu@naver.com>
2022-06-09 14:48:56 +0900

    편의변경사항

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function onFormSubmit() {
 function readFormData() {
     var formData = {};
     formData["fullName"] = document.getElementById("fullName").value;
-    formData["email"] = document.getElementById("email").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    formData["callNumber"] = document.getElementById("callNumber").value;
+    formData["age"] = document.getElementById("age").value;
+    formData["sex"] = document.getElementById("sex").value;
     return formData;
 }
 
@@ -26,11 +26,11 @@ function insertNewRecord(data) {
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fullName;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
+    cell2.innerHTML = data.callNumber;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.age;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
+    cell4.innerHTML = data.sex;
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
@@ -38,24 +38,24 @@ function insertNewRecord(data) {
 
 function resetForm() {
     document.getElementById("fullName").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
+    document.getElementById("callNumber").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("sex").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("callNumber").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("sex").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.email;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[1].innerHTML = formData.callNumber;
+    selectedRow.cells[2].innerHTML = formData.age;
+    selectedRow.cells[3].innerHTML = formData.sex;
 }
 
 function onDelete(td) {
@@ -76,4 +76,22 @@ function validate() {
             document.getElementById("fullNameValidationError").classList.add("hide");
     }
     return isValid;
+}
+
+function filter(){
+
+    var value, name, item, i;
+
+    value = document.getElementById("value").value.toUpperCase();
+    FullName = document.getElementsByClassName("item");
+
+    for(i=0;i<item.length;i++){
+        name = item[i].getElementsByClassName("item");
+        if(name[0].innerHTML.toUpperCase().indexOf(value) > -1){
+            item[i].style.display = "flex";
+  }
+        else{
+            item[i].style.display = "none";
+  }
+}
 }
